50d79ba0a11038a571fcd232ff437deb34e57c1b
Salmon <salmonbekqahhorov@gmail.com>
2026-03-13 11:02:51 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+
+
+db_host = os.getenv("MYSQLHOST")
+if not db_host:
+    print("⚠️ DIQQAT: MYSQLHOST topilmadi! Railway Variables-ni tekshiring.")
 
 DB_CONFIG = {
-    "host": os.getenv("MYSQLHOST"),
+    "host": db_host,
     "user": os.getenv("MYSQLUSER"),
     "password": os.getenv("MYSQLPASSWORD"),
     "database": os.getenv("MYSQLDATABASE"),
